1561c3c83f7ac34b4fb354af51aae889943ad5c5
Oryan <oryanagam@gmail.com>
2021-04-12 13:23:17 +0300

    1322

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,9 +14,9 @@ export interface Tile {
 })
 export class HomeComponent implements OnInit {
   tiles: Tile[] = [
-    {text: 'Welcome to MyCar!', cols: 3, rows: 1, color: 'lightpink'},
-    {text: '???!?!!?', cols: 1, rows: 1, color: 'lightpink'},
-    {text: '(C) Oryan, Eyal & Tal', cols: 2, rows: 1, color: '#f5bfbf'},
+    {text: 'Welcome to MyCar!', cols: 3, rows: 1, color: '#CD5C5C'},
+    {text: 'April 2021', cols: 1, rows: 1, color: '#F08080'},
+    {text: '(C) Oryan, Eyal & Tal', cols: 2, rows: 1, color: '#E9967A'},
   ];
 
   constructor() { }
